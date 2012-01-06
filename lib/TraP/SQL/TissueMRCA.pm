9bf1abe94b06463b4be66d2bfae902a878015b7b
OwenRackham <owen.rackham@gmail.com>
2012-01-06 11:51:25 +0000

    changed a couple of things so the MRCA works

--- lib/TraP/SQL/TissueMRCA.pm
+++ lib/TraP/SQL/TissueMRCA.pm
@@ -10,11 +10,16 @@ our %EXPORT_TAGS = (
 'all' => [ qw(
         human_cell_type_experiments
 		experiment_sfs
+		experiment_supras
 		sf_genomes
+		supra_genomes
 		all_sfs
+		all_supras
 		calculate_MRCA_NCBI_placement
 		taxon_histogram
 		calculateMRCAstats
+		experiment_protein_genedistance
+		all_protein_genedistance
 ) ],
 );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
@@ -100,11 +105,16 @@ Function to get all the human cell type experiment ids
 sub human_cell_type_experiments {
 	my @ids = ();
 	my $dbh = dbConnect('trap');
-	my $sth = $dbh->prepare('select experiment_id from experiment where source_id = ? limit 1');
+	my $sth = $dbh->prepare('select experiment_id from experiment where source_id = ?');
 	$sth->execute(1);
-	my $results = $sth->fetchall_arrayref();
-	dbDisconnect($dbh);
-	return $results->[0];
+	
+	my $results=[];	
+	while(my ($exp_id)=  $sth->fetchrow_array()){
+		push(@$results,$exp_id)
+	}
+
+	dbDisconnect($dbh);	
+	return $results;
 }
 
 =item * sf_genomes
@@ -117,7 +127,7 @@ sub sf_genomes {
     foreach my $sf (@$ListOfSuperfamilies){
     	
  	   my $dbh = dbConnect('superfamily');
-    	my $sth = $dbh->prepare("SELECT distinct len_supra.genome FROM genome,len_supra,comb_index WHERE comb_index.length = 1 AND comb_index.comb = ? AND comb_index.id=len_supra.supra_id AND genome.genome=len_supra.genome AND genome.include='y';");
+    	my $sth = $dbh->prepare("SELECT distinct len_supra.genome FROM genome,len_supra,comb_index WHERE comb_index.length = 1 AND comb_index.comb = ? AND comb_index.id=len_supra.supra_id AND genome.genome=len_supra.genome AND genome.include= 'y';");
 
  	   $sth->execute($sf);
     
@@ -127,6 +137,29 @@ sub sf_genomes {
     }
 
 	return \%sf_genomes;
+
+}
+
+=item * supra_genomes
+Function to find all the genomes a supradomain occurs in given an array ref of a list of supra ids. Returns a hash of $HAsh->{SFid}=[list of genomes]
+=cut
+sub supra_genomes {
+    
+    my ($ListOfSupraIDs) = @_;
+    my %supra_genomes;
+    foreach my $supra (@$ListOfSupraIDs){
+    	
+ 	   my $dbh = dbConnect('superfamily');
+    	my $sth = $dbh->prepare("select distinct(len_supra.genome) from len_supra,genome where genome.genome = len_supra.genome and len_supra.supra_id = ? and genome.include = 'y';");
+
+ 	   $sth->execute($supra);
+    
+    	while ( my ($genome) = $sth->fetchrow_array() ) {
+    		push (@{$supra_genomes{$supra}},$genome);
+    	}
+    }
+
+	return \%supra_genomes;
 
 }
 
@@ -184,6 +217,85 @@ return \@sfs;
 }
 
 
+=item * experiment_supras
+For a given sampleID this returns an array of disitinct supraIDs where protein|_ascomb > 0 that are expressed in that experiment
+
+At current, the supraIDs returned are those found in homosapien 'hs'
+=cut
+sub experiment_supras {
+
+my $sample = shift;
+my @supras;
+my ($dbh, $sth);
+print "connected\n";
+$dbh = dbConnect();
+
+$sth =   $dbh->prepare( "select distinct(len_supra.supra_id) from genome,comb,len_supra,(select distinct(protein)as p from 
+trap.cell_snapshot, trap.id_mapping where trap.cell_snapshot.gene_id = trap.id_mapping.entrez and trap.cell_snapshot.experiment_id 
+= $sample) as a where comb.protein=a.p and len_supra.ascomb_prot_number > 0 and comb.comb_id=len_supra.supra_id AND len_supra.genome = 'hs'
+ and len_supra.genome = genome.genome and genome.include = 'y';" );
+        	$sth->execute;
+        	while (my ($supra) = $sth->fetchrow_array ) {
+				push @supras, $supra;
+        	}
+        	print "run\n";
+        	
+dbDisconnect($dbh);
+return \@supras;
+}
+
+=item * experiment_protein_genedistance
+For a given experiment this returns an hash of protein ids limked to their average gene distance according to mogrify.
+=cut
+sub experiment_protein_genedistance {
+
+my $sample = shift;
+my $cutoff = shift;
+my %gene_distances;
+my ($dbh, $sth);
+$dbh = dbConnect();
+$sth =   $dbh->prepare( " select distinct(protein) as p, max(trap.cell_snapshot.`mogrify_gene_distance`) from trap.cell_snapshot, 
+trap.id_mapping where trap.cell_snapshot.gene_id = trap.id_mapping.entrez and trap.cell_snapshot.`mogrify_gene_distance` > $cutoff 
+and trap.cell_snapshot.experiment_id = $sample group by p;" );
+        	$sth->execute;
+        	while (my ($protein,$gene_distance) = $sth->fetchrow_array ) {
+        		if($gene_distance >= $cutoff){
+				$gene_distances{$protein} = $gene_distance;
+        		}
+        	}
+dbDisconnect($dbh);
+return \%gene_distances;
+}
+
+=item * all_protein_genedistance
+For a given source this returns an hash of protein ids limked to their average gene distance according to mogrify.
+=cut
+sub all_protein_genedistance {
+
+my $source = shift;
+my $cutoff = shift;
+my %gene_distances;
+my ($dbh, $sth);
+$dbh = dbConnect();
+$sth =   $dbh->prepare( " select distinct(protein) as p, max(trap.cell_snapshot.`mogrify_gene_distance`) from trap.cell_snapshot, 
+trap.id_mapping where trap.cell_snapshot.gene_id = trap.id_mapping.entrez and trap.cell_snapshot.`mogrify_gene_distance` > $cutoff group by p;" );
+        	$sth->execute;
+        	while (my ($protein,$gene_distance) = $sth->fetchrow_array ) {
+        		if($gene_distance >= $cutoff){
+        			if(exists($gene_distances{$protein})){
+        			unless($gene_distances{$protein}>$gene_distance){
+						$gene_distances{$protein} = $gene_distance;
+        			}
+        			}else{
+        				$gene_distances{$protein} = $gene_distance;
+        			}
+        		}
+        	}
+dbDisconnect($dbh);
+return \%gene_distances;
+}
+
+
 =item * all_sfs
 For a given source_id this returns an array of distinct sfs that are expressed in any experiment in that source
 =cut
@@ -194,13 +306,46 @@ my @sfs;
 my ($dbh, $sth);
 $dbh = dbConnect();
 
-$sth =   $dbh->prepare( "select distinct(superfamily.ass.sf) from trap.cell_snapshot, trap.id_mapping, superfamily.ass,trap.experiment where trap.cell_snapshot.gene_id = trap.id_mapping.entrez and trap.id_mapping.protein = superfamily.ass.protein and trap.experiment.experiment_id = trap.cell_snapshot.experiment_id and trap.experiment.source_id = $source;");
+$sth =   $dbh->prepare( "select distinct(superfamily.ass.sf) from trap.cell_snapshot, trap.id_mapping, superfamily.ass,trap.experiment 
+where trap.cell_snapshot.gene_id = trap.id_mapping.entrez and trap.id_mapping.protein = superfamily.ass.protein and 
+trap.experiment.experiment_id = trap.cell_snapshot.experiment_id and trap.experiment.source_id = $source;");
         	$sth->execute;
         	while (my ($sf) = $sth->fetchrow_array ) {
 				push @sfs, $sf;
         	}
 dbDisconnect($dbh);
 return \@sfs;
+}
+
+=item * all_supras
+For a given source_id this returns an array of distinct supras that are expressed in any experiment in that source
+
+At current, the supraIDs returned are those found in homosapien 'hs'
+=cut
+sub all_supras {
+
+my $source = shift;
+my @supras;
+my ($dbh, $sth);
+my %supras;
+my %prot_lookup;
+my %supra_lookup;
+$dbh = dbConnect();
+
+$sth =   $dbh->prepare( "select distinct(len_supra.supra_id),p from genome,comb,len_supra,(select distinct(protein)as p from 
+trap.cell_snapshot, trap.id_mapping,trap.experiment where trap.cell_snapshot.gene_id = trap.id_mapping.entrez and 
+trap.experiment.experiment_id = trap.cell_snapshot.experiment_id and trap.experiment.source_id = 1) as a where comb.protein=a.p 
+and len_supra.ascomb_prot_number > 0 and comb.comb_id=len_supra.supra_id AND genome.genome = 'hs' and len_supra.genome = genome.genome
+ and genome.include = 'y';");
+        	$sth->execute;
+        	while (my ($supra,$protein) = $sth->fetchrow_array ) {
+				$supras{$supra} =1;
+				$prot_lookup{$protein} = $supra;
+				push(@{$supra_lookup{$supra}},$protein);
+        	}
+ @supras = keys %supras;
+dbDisconnect($dbh);
+return (\@supras,\%prot_lookup,\%supra_lookup);
 }
 
 
@@ -274,7 +419,7 @@ sub calculate_MRCA_NCBI_placement{
 	my $SF2MRCAHash = {};
 	my $TaxonID2leftrightidDictionary = {};
 	
-	$sth = $dbh->prepare("SELECT ncbi_taxonomy.taxon_id, ncbi_taxonomy.name,ncbi_taxonomy.rank, tree.left_id, tree.right_id FROM tree JOIN ncbi_taxonomy ON ncbi_taxonomy.taxon_id = tree.taxon_id WHERE tree.left_id = (SELECT MAX(tree.left_id) FROM tree WHERE tree.left_id <= ? AND tree.right_id >= ?);");
+	$sth = $dbh->prepare("SELECT tree.left_id, tree.right_id FROM tree WHERE tree.left_id = (SELECT MAX(tree.left_id) FROM tree WHERE tree.left_id <= ? AND tree.right_id >= ?);");
 	
 	my $MaxLeftID = List::Util::min(@$Genome_left_ids);
 	my $MinRightID = List::Util::max(@$Genome_right_ids);
@@ -291,11 +436,11 @@ sub calculate_MRCA_NCBI_placement{
 		die "Query appears to have failed on left_id $MaxLeftID and right_id $MinRightID!\n";
 	}
 					
-	my ($taxon_id,$name,$rank,$MRCAleftid,$MRCArightid) = $sth->fetchrow_array;
+	my ($MRCAleftid,$MRCArightid) = $sth->fetchrow_array;
 	
 	$sth->finish;
 	
-	my $DistanceFromReference; #This is the distance on the tree (in aggregated branch lengths) from MRCA
+	my ($DistanceFromReference,$NCBIPlacement); #This is the distance on the tree (in aggregated branch lengths) from MRCA
 	
 	unless($ReferenceDistanceGenome ~~ undef){
 		#i.e. if a reference distance genome was provided
@@ -309,6 +454,8 @@ sub calculate_MRCA_NCBI_placement{
 			
 			($DistanceFromReference) = $sth->fetchrow_array;
 			
+			$DistanceFromReference = 0 if($DistanceFromReference ~~ undef);
+			
 			$sth->finish;
 			
 		}else{
@@ -317,11 +464,19 @@ sub calculate_MRCA_NCBI_placement{
 			#Check that what we're doing is sensible
 		}
 		
+		$sth = $dbh->prepare("SELECT ncbi_taxonomy.name FROM ncbi_taxonomy JOIN tree ON tree.left_id = ncbi_taxonomy.left_id WHERE tree.left_id = ? AND tree.right_id = ?;");
+		
+		$sth->execute($MRCAleftid,$MRCArightid);
+		
+		($NCBIPlacement) = $sth->fetchrow_array;
+			
+		$sth->finish;
+		
 	}	
 	
 	dbDisconnect($dbh);
 	
-	return ($taxon_id,$name,$rank,$DistanceFromReference);
+	return ($DistanceFromReference,$NCBIPlacement);
 }
 
 =item * calculateMRCAstats
@@ -341,8 +496,9 @@ sub calculateMRCAstats {
 	
 	$ReferenceDistanceGenomes = 'hs' if($ReferenceDistanceGenomes ~~ undef); #Set a default of homo sampien
 	
-	my $Supra2TreeData = {};
-	#Structure will be $Supra2TreeData->{$supra_id}=[$MRCAtaxon_id,$MRCA_NCBI_Taxonomy_Name,$MRCA_NCBI_Taxonomy_Rank,$DistanceFromReference]
+	my $Supra2TreeDistData = {};
+	my $Supra2TreePlacemenetData = {};
+	#Structure will be $Supra2TreeDistData->{$supra_id}=[$MRCAtaxon_id,$MRCA_NCBI_Taxonomy_Name,$MRCA_NCBI_Taxonomy_Rank,$DistanceFromReference]
 
 	foreach my $Trait (keys(%$Trait2GenomesHash)){
 		
@@ -350,13 +506,15 @@ sub calculateMRCAstats {
 		
 		print STDERR "Reference Distance genome $ReferenceDistanceGenomes not in list of genomes passed in for Trait $Trait\n" unless(grep{$_ =~ /$ReferenceDistanceGenomes/}@TraitGenomes);
 		
-		my ($taxon_id,$name,$rank,$DistanceFromReference) = calculate_MRCA_NCBI_placement(\@TraitGenomes,$ReferenceDistanceGenomes);
-		$Supra2TreeData->{$Trait}=[$taxon_id,$name,$rank,$DistanceFromReference];
+		my ($DistanceFromReference,$NCBIPlacement) = calculate_MRCA_NCBI_placement(\@TraitGenomes,$ReferenceDistanceGenomes);
+		$Supra2TreeDistData->{$Trait}=$DistanceFromReference;
+		$Supra2TreePlacemenetData->{$Trait}=$NCBIPlacement;
+		
 	}
 	
-	
-	return ($Supra2TreeData);
+	return ($Supra2TreeDistData,$Supra2TreePlacemenetData);
 }
+
 
 1;
 __END__
