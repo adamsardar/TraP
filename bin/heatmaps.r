94c6fdde51ac2e62ca2270b8a610daf4ce42570b
Adam Sardar <adam@sardar.me.uk>
2012-12-20 22:29:02 +0000

    Changed heatmap generation so as to include violin plots

--- bin/heatmaps.r
+++ bin/heatmaps.r
@@ -19,17 +19,64 @@ setwd("~/TraP/data")
 Zscores <- read.delim("./matrix.tab", sep = "\t",header=FALSE)
 rownames(Zscores)<-as.matrix(read.delim("./cols.tab", sep = "\t",header=FALSE))
 colnames(Zscores)<-as.matrix(read.delim("./rows.tab", sep = "\t",header=FALSE))
-Zscores<-Zscores[,-22]
+Zscores<-Zscores[,-14]
 #file seems to contain a whole load of 'NA' entries
 
-cosZscores<-cosine(as.matrix(t(Zscores)))
 
-data <- read.delim("./Intersection-AllvsAllvsAllSummary.dat", sep = "\t")
+boxplot(Zscores[c("Homo sapiens",
+"Theria",
+"Mammalia",
+"Amniota",
+"Euteleostomi",
+"Chordata",
+"Deuterostomia",
+"Coelomata",
+"Bilateria",
+"Eumetazoa",
+"Opisthokonta",
+"Eukaryota",
+"CellularOrganisms"
+)],
 
+
+par(cex=0.8,las=3)
+vioplot(Zscores$"Homo sapiens",
+Zscores$"Theria",
+Zscores$"Mammalia",
+Zscores$"Amniota",
+Zscores$"Euteleostomi",
+Zscores$"Chordata",
+Zscores$"Deuterostomia",
+Zscores$"Coelomata",
+Zscores$"Bilateria",
+Zscores$"Eumetazoa",
+Zscores$"Opisthokonta",
+Zscores$"Eukaryota",
+Zscores$"CellularOrganisms",
+names=c("Homo sapiens",
+"Theria",
+"Mammalia",
+"Amniota",
+"Euteleostomi",
+"Chordata",
+"Deuterostomia",
+"Coelomata",
+"Bilateria",
+"Eumetazoa",
+"Opisthokonta",
+"Eukaryota",
+"CellularOrganisms"), wex=1.2, col='lightblue'
+)
+
+title(main="Violin Plot Of Epoch Comb Z-scores Collapsed to 13 Taxonomy Points")
+        
 source("../bin/A2Rcode.r")
-
+        
+library("lsa")
+cosZscores<-cosine(as.matrix(t(Zscores)))
+        
 # prepare hierarchical cluster
-hc = hclust(dist(Zscores),method="ward")
+hc = hclust(dist(cosZscores))
 
 # load code of A2R function
 #source("http://addictedtor.free.fr/packages/A2R/lastVersion/R/code.R")
@@ -37,7 +84,7 @@ hc = hclust(dist(Zscores),method="ward")
 
 
 op = par(bg="white")
-A2Rplot(hc, k=15, boxes = FALSE, show.labels=TRUE,
-        col.up = "gray50", col.down = c("blue4","brown","burlywood3","blue2","cyan","darkcyan","darkorchid3","black","chartreuse4","red1","darkgoldenrod1","black","blue","red","grey"))
+A2Rplot(hc, k=24, boxes = FALSE, show.labels=TRUE,
+   col.up = "gray50", col.down = c("black","chartreuse4","red1","blue","darkgoldenrod1","black","red","grey","black","chartreuse4","blue","red1","darkgoldenrod1","black","red","grey","black","chartreuse4","blue","red1","darkgoldenrod1","black","red","grey"))
 par(op)
 
