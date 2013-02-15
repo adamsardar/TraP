a3641b0a9990ddfa75767aa0428901aa809eab64
Adam Sardar <adam@sardar.me.uk>
2013-02-15 01:48:02 +0000

    Finally finished epoch_percentages_file

--- bin/epoch_percentage_composition.pl
+++ bin/epoch_percentage_composition.pl
@@ -120,6 +120,7 @@ my $help;    #Same again but this time should we output the POD man page defined
 my $samplesfile;
 my $translation_file;
 my $union = 0;
+my $removeubiq = 0;
 
 #Set command line flags and parameters.
 GetOptions("verbose|v!"  => \$verbose,
@@ -128,6 +129,7 @@ GetOptions("verbose|v!"  => \$verbose,
            "taxontranslate|tr:s" => \$translation_file,
            "samples|s=s" => \$samplesfile,
            "union|u!" => \$union,
+           "removeubiq|r!" => \$removeubiq,
         ) or die "Fatal Error: Problem parsing command-line ".$!;
 
 #Get other command line arguments that weren't optional flags.
@@ -191,55 +193,119 @@ open TIMEPERCENTAGES, ">../data/EpochSampleGroupPercentages.dat" or die $!."\t".
 print TIMEPERCENTAGES join("\t",@SortedEpochs);
 print TIMEPERCENTAGES "\n";
 
-
-
-
-$sth = $dbh->prepare("SELECT DISTINCT(comb_id) 
-						FROM snapshot_order_comb
-						WHERE sample_id = ?");
+$sth = $dbh->prepare("SELECT DISTINCT sample_id,comb_id 
+						FROM snapshot_order_comb;");
 						
 $tth = $ebh->prepare("SELECT comb_MRCA.taxon_id
 						FROM comb_MRCA
 						WHERE comb_id = ?");
 
-while(my $line = <SAMPLEIDS>){
+my $SampleID2Combs = {};
+#Grab a list of comb ids per sample and whack them into a hash
+
+#So as to speed up execution, dump that hash to a file, unless we've already done so. In which case, use it!
+
+unless(-e "/tmp/SampleID2Combs.dat" && ! $debug){
+
+	print STDERR "Creating the hash SampleID2combs.dat and dumping it to file ...";
 	
-	chomp($line);
-	my ($comment,$samids) = split(/\s+/,$line);
-	my @sampleids = split(',',$samids);
-	
-	my $SampleID2Combs = {};
-	#Grab a list of comb ids per sample and whack them into a hash
-	
-	foreach my $sample_id (@sampleids){
-	
-		$sth->execute($sample_id);
-		
-		while (my ($comb_id) = $sth->fetchrow_array()){
+	$sth->execute();
 			
-			$SampleID2Combs->{$sample_id}=[] unless(exists($SampleID2Combs->{$sample_id}));
-			push(@{$SampleID2Combs->{$sample_id}},$comb_id);
-		}
+	while (my ($sample_id,$comb_id) = $sth->fetchrow_array()){
+				
+				$SampleID2Combs->{$sample_id}=[] unless(exists($SampleID2Combs->{$sample_id}));
+				push(@{$SampleID2Combs->{$sample_id}},$comb_id);
 	}
 
-	my $lc = List::Compare->new( {
-        lists    => [(values(%$SampleID2Combs))],
-        unsorted => 1,
-    } );
+	EasyDump("/tmp/SampleID2Combs.dat",$SampleID2Combs);
+
+
+	print STDERR " done.\n";
+
+}else{
+	
+	print STDERR "Using a dump of the hash SampleID2combs.dat from an earlier run ...";
+	$SampleID2Combs = EasyUnDump("/tmp/SampleID2Combs.dat");
+	print STDERR " loaded.\n";
+}
+
+
+#If requested (using -r or --removeubiq), make a lsit of DAs that exists in ALL samples
+my @UbiqCombs;
+if($removeubiq){
+	
+	unless(-e "/tmp/Ubiqcombs.dat" && ! $debug){
+
+		print STDERR "Creating the array UbiqCombs and dumping it to file ...";
+		my $TotalSampleCompare = List::Compare->new( {
+			        lists    => [(@{$SampleID2Combs}{keys(%$SampleID2Combs)})],
+			        unsorted => 1,
+			        accelerated => 1,
+			    });
+	
+		@UbiqCombs = $TotalSampleCompare->get_intersection;
+		EasyDump("/tmp/Ubiqcombs.dat",\@UbiqCombs);
+		print STDERR " done.\n";
+		
+	}else{
+		
+		print STDERR "Using a dump of the uniqutous domains from an earlier run ...";
+		my $tmp = EasyUnDump("/tmp/Ubiqcombs.dat");
+		@UbiqCombs = @$tmp;
+		print STDERR " loaded.\n";
+	}
+	
+	print STDERR "Number of ubiquitous domain archs:".scalar(@UbiqCombs)." - these shall be removed from the sample sets that you have inputted\n";
+}
+
+#For each line of the input file, loop through, grab a list of sample ids and then work out what is in the interection of all of their comb ids
+while(my $line = <SAMPLEIDS>){
+	
+	print STDERR "Processing line $. of input ... \n";
+	
+	chomp($line);
+	my ($comment,$samids) = split(/\t+/,$line);
+	my @sampleids = split(',',$samids);
 	
 	my @DistinctCombIDs;
 	
-	unless($union){
+	unless(scalar(@sampleids) == 1){
+
+		my $lc = List::Compare->new( {
+	        lists    => [(@{$SampleID2Combs}{@sampleids})],
+	        unsorted => 1,
+	        accelerated => 1,
+	    } );
 		
-		@DistinctCombIDs = $lc->get_intersection;
+		unless($union){
+			
+			@DistinctCombIDs = $lc->get_intersection;
+		}else{
+			
+			@DistinctCombIDs = $lc->get_union;
+		}
+		
 	}else{
 		
-		@DistinctCombIDs = $lc->get_union;
+		@DistinctCombIDs = @{@{$SampleID2Combs->{$sampleids[0]}}};
+	}
+	
+	#If asked to remove DAs that are present in all samples, we do that here
+	if($removeubiq){
+		
+		my $removallc = List::Compare->new( {
+	        lists    => [\@DistinctCombIDs,\@UbiqCombs],
+	        unsorted => 1,
+	        accelerated => 1,
+	    } );
+	    
+	    @DistinctCombIDs = $removallc->get_Lonly;;
 	}
 	
 	my $TaxID2DomArchCountHash ={};
 	my $DistinctDAcount=scalar(@DistinctCombIDs);
 	
+	#Get the MRCA of each and every comb
 	foreach my $DA (@DistinctCombIDs){
 		
 		$tth->execute($DA);
@@ -254,10 +320,9 @@ while(my $line = <SAMPLEIDS>){
 		#Finally, uodate the hash
 	}
 	
-
-	
 	my $CumlativeEpochCount = 0;
 	
+	#Now for the output. For each epoch, print the percentage of our DA set that comes about at that time, alongside the rolling cumulative
 	print TIMEPERCENTAGES $comment."\t";
 	foreach my $Epoch (@SortedEpochs){
 		
